ce5c5002736086ec048cbfc7ffe994c9719af85f
GowthamBaddi123 <116878326+GowthamBaddi123@users.noreply.github.com>
2025-03-15 12:51:16 +0530

    index css error fixed

--- Client/src/components/LandingPage/Landpage.jsx
+++ Client/src/components/LandingPage/Landpage.jsx
@@ -2,31 +2,29 @@ import HomeBanner from "../../assets/podcast_Banner.jpg";
 import guyWithhandonHead from "../../assets/guy-with-chaos-head-01.png";
 import Categories from "../../Utils/Categories.jsx";
 import greenTickImage from "../../assets/checked.png";
-import "./index.css.css";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ListeningImage from "../../assets/listening.png";
 import Navbar from "../Navbar.jsx";
-import Footer from "../Footer.jsx"
-
+import Footer from "../Footer.jsx";
 
 const LandingPage = () => {
   return (
     <>
-      
       <Navbar />
 
       {/* home */}
       <div className="home">
-        <img src={HomeBanner} alt="Home Banner"  id="home"/>
+        <img src={HomeBanner} alt="Home Banner" id="home" />
 
         {/* About */}
         <div id="about">
           <div>
             <h2 className="heading">About Us</h2>
             <p className="description">
-              Welcome to <strong>PodListen</strong>, your ultimate destination for discovering,
-              streaming, and enjoying podcasts from around the world. At
-              PodListen, we believe in the power of storytelling,
+              Welcome to <strong>PodListen</strong>, your ultimate destination
+              for discovering, streaming, and enjoying podcasts from around the
+              world. At PodListen, we believe in the power of storytelling,
               knowledge-sharing, and the magic of audio content to inspire,
               educate, and entertain.
             </p>
@@ -61,7 +59,7 @@ const LandingPage = () => {
 
         {/* Categories */}
         <h2 id="categories">Popular Podcast Categories</h2>
-        <Categories loggedin={false}/>
+        <Categories loggedin={false} />
 
         {/* footer */}
         <Footer />
