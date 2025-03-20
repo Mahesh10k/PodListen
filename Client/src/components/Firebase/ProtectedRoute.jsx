990e38321d44ad12ebb01e07fdda7ca085fe9653
gowthambaddi123 <gowthambaddi254@gmail.com>
2025-03-21 00:19:50 +0530

    installed react-firebase-hooks

--- Client/src/components/Firebase/ProtectedRoute.jsx
+++ Client/src/components/Firebase/ProtectedRoute.jsx
@@ -1,18 +1,17 @@
 // ProtectedRoute.js
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
+import {useAuthState} from 
 import { auth } from "./firebase"; 
 
 const ProtectedRoute = ({ children }) => {
   const [user] = useAuthState(auth); 
   
   if (!user) {
-    return <Navigate to="/login" />;  // If not logged in, redirect to the login page
+    return <Navigate to="/login" />;  
   }
 
   if (user.email !== "maheshpikki03@gmail.com") {
-    // If the email is not the allowed one, redirect to the Unauthorized page
     return <Navigate to="/unauthorized" />;
   }
 
